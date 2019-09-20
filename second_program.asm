2ef0b7760c9ddae35928dda00ef166f8acc20266
Luis Leon <leon.luisgerardo775@gmail.com>
2019-09-20 18:29:32 -0500

    Second program

---
+++ second_program.asm
@@ -0,0 +1,20 @@
+ RADIX HEX
+
+; ------------------ DATOS DEL PROGRAMA ------------------
+    W EQU 0
+    ACS EQU 0 ;direcciona a una localidad de memoria limitada
+    BS equ 1 ;direcciona a todas las localidades de memorias disponibles en la RAM
+ 
+    UDATA 0X060 ;memoria de datos
+    OPER1 RES 1 ;reserva una localidad de memoria
+    OPER2 RES 1
+    RESUL RES 1
+; ------------------ CODIGO ------------------
+    CODE 0X060 ;memoria de programa
+    movlw 0x20
+    movwf OPER1, BS
+    movlw 0x26
+    movwf OPER2, BS
+    addwf OPER1, W, BS
+    movwf RESUL, BS
+    END
